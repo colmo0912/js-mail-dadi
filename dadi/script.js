a2c8f0d80aa96ffb176008e88a240918d7333493
colmo0912 <colmo0912@gmail.com>
2021-04-09 16:11:36 +0200

    inserisco if/else

--- dadi/script.js
+++ dadi/script.js
@@ -4,6 +4,17 @@ console.log(randomUtente);
 var randomPC = Math.floor(Math.random()*7);
 console.log(randomPC);
 
+if (randomUtente>randomPC){
+
+document.getElementById("punteggio").innerHTML="Vince Utente"
+}else if(randomUtente===randomPC){
+
+    document.getElementById("punteggio").innerHTML="Pareggio"
+}
+else {
+    document.getElementById("punteggio").innerHTML="Vince PC"
+}
+
 
 
 
