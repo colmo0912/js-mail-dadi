df9d14e29809dd0c0c61b0f2fb33ded9f786563e
colmo0912 <colmo0912@gmail.com>
2021-04-09 15:16:20 +0200

    aggiungo variabili

---
+++ mail/script.js
@@ -0,0 +1,6 @@
+// scrivo elenco di mail e la mail dell'utente
+
+var elencoMail = ["index@gmail.com", "script@gmail.com" , "andrea@gmail.com" , "boolean@gmail.com" ];
+var mailUtenteCorretta = "andrea@gmail.com";
+var mailInput =prompt("Inserisci la tua mail e verifica se puoi accedere")
+
